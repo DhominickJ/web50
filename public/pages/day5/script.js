4bc5caf2062615bbe7141d83811c0bf02b1b64c4
DhominickJ <dhominickjohn.billena@wvsu.edu.ph>
2025-01-30 23:53:18 +0800

    Adding day5 blurring background progress

--- public/pages/day5/script.js
+++ public/pages/day5/script.js
@@ -12,14 +12,19 @@ function blurring() {
         clearInterval(int)
     }
 
-    console.log(load)
+    // console.log(load)
     loadText.innerText = `${load}%`;
-    // loadText.style.opacity = scale(load, 0, 100, 1, 0);
-    console.log(scale(load, 0, 100, 0, 1))
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+    // console.log(scale(load, 0, 100, 0, 1))
 }
 
 let scale = (num, in_min, in_max, out_min, out_max) => {
+    // val1 = (num - in_min)
+    // val2 = (out_max - out_min)
+    // val3 = (in_max - in_min)
+    // val4 = out_min
     value = (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
-    console.log(num, in_min, in_max, out_min, out_max)
+    // console.log()
     return value
 }
